616fe8b1b9f107e4330e405ebe1f328495e5ae3b
Stacy Brothers <bro18063@byui.edu>
2018-09-28 20:58:57 -0600

    having to push every time is irritating

--- web/index.php
+++ web/index.php
@@ -1,7 +1,12 @@
-<html><body><?php
-
-  echo "testing";
-
-?>
+<html>
+    <head>
+        <title>CS 313 Assignments Portfolio</title>
+        <link rel="stylesheet" type="text/css" href="index.css">
+    </head>
+    <body>
+        <div class="header">CS 313</div>
+        <div>
+            
+        </div>
     </body>
 </html>
