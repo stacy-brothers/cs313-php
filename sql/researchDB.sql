4afda1d1afd32a1cec179cd00aebffdd7a8560dd
Stacy Brothers <bro18063@byui.edu>
2018-10-11 20:42:04 -0600

    forgot my semicolons

--- sql/researchDB.sql
+++ sql/researchDB.sql
@@ -9,7 +9,7 @@ create table researcher (
     fullname varchar(50),
     email varchar(50),
     pass_hash varchar(254)
-)
+);
 
 -- this is the main table 
 create table topic (
@@ -18,7 +18,7 @@ create table topic (
     researcher_id references researcher(id),
     private boolean default false,
     notes text
-)
+);
 
 -- this is a reference url for a particular topic.  
 -- one topic may have many reference urls
@@ -27,19 +27,19 @@ create table ref_url (
     url varchar(254) not null, 
     descr varchar(254),
     topic_id integer references topic(id)
-)
+);
 
 -- this is a lookup table that contains keywords that can be assigned to topics
 create table keyword ( 
     id serial primary key,
     keyword varchar(30) not null unique
-)
+);
 
 -- this is the xref table for the many-to-many keyword to topic relationship
 create table topic_keyword (
     topic_id integer references topic(id),
     keyword_id integer references keyword(id)
-)
+);
 
 
 
