c21018231973d990ecf304cf8a782aeb380b5f51
Stacy Brothers <bro18063@byui.edu>
2018-10-11 20:22:22 -0600

    added researcher

--- sql/researchDB.sql
+++ sql/researchDB.sql
@@ -1,8 +1,21 @@
 
+create database research;
+
+-- users of the database
+create table researcher (
+    id serial primary key,
+    username varchar(30),
+    fullname varchar(50),
+    email varchar(50),
+    pass_hash varchar(254)
+)
+
 -- this is the main table 
 create table topic (
     id serial primary key,
     topic varchar(254) not null,
+    researcher_id references researcher(id),
+    private boolean default false,
     notes text
 )
 
@@ -18,7 +31,7 @@ create table ref_url (
 -- this is a lookup table that contains keywords that can be assigned to topics
 create table keyword ( 
     id serial primary key,
-    keyword varchar(30) not null
+    keyword varchar(30) not null unique
 )
 
 -- this is the xref table for the many-to-many keyword to topic relationship
