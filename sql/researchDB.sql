d21fcfe7d11a59265108013b50e7b7c332885ffe
Stacy Brothers <bro18063@byui.edu>
2018-10-11 20:39:38 -0600

    fixing comments

--- sql/researchDB.sql
+++ sql/researchDB.sql
@@ -1,8 +1,8 @@
 
-# create database research;
-# no permissions to create table in heroku postgres.... :(
+-- create database research;
+-- no permissions to create table in heroku postgres.... :(
 
-# users of the database
+-- users of the database
 create table researcher (
     id serial primary key,
     username varchar(30),
@@ -11,7 +11,7 @@ create table researcher (
     pass_hash varchar(254)
 )
 
-# this is the main table 
+-- this is the main table 
 create table topic (
     id serial primary key,
     topic varchar(254) not null,
@@ -20,8 +20,8 @@ create table topic (
     notes text
 )
 
-# this is a reference url for a particular topic.  
-# one topic may have many reference urls
+-- this is a reference url for a particular topic.  
+-- one topic may have many reference urls
 create table ref_url (
     id serial primary key,
     url varchar(254) not null, 
@@ -29,13 +29,13 @@ create table ref_url (
     topic_id integer references topic(id)
 )
 
-# this is a lookup table that contains keywords that can be assigned to topics
+-- this is a lookup table that contains keywords that can be assigned to topics
 create table keyword ( 
     id serial primary key,
     keyword varchar(30) not null unique
 )
 
-# this is the xref table for the many-to-many keyword to topic relationship
+-- this is the xref table for the many-to-many keyword to topic relationship
 create table topic_keyword (
     topic_id integer references topic(id),
     keyword_id integer references keyword(id)
