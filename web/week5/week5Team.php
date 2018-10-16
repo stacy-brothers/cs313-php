890ba0824ad6b964e51db3cf68c2def75c3d58d6
Stacy Brothers <bro18063@byui.edu>
2018-10-15 22:41:10 -0600

    week 5 team stuff

--- web/week5/week5Team.php
+++ web/week5/week5Team.php
@@ -27,7 +27,6 @@ catch (PDOException $ex)
     <head>
         <title>CS 313 Assignments Portfolio</title>
         <link rel="stylesheet" type="text/css" href="../index.css">
-        <link rel="stylesheet" type="text/css" href="./week5.css">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
     </head>
     <body>
@@ -39,8 +38,6 @@ catch (PDOException $ex)
             </div>
             <div class="content">
                 <div class="page-title">Scripture Resources</div>
-            </div>
-            <div>
                 <?php
                 foreach ($db->query('SELECT book, chapter, verse, content FROM scripture') as $row) {
                     echo '<div><b>' . $row['book'] . ' ' . $row['chapter'] . ':' . $row['verse'] . '</b> - ' . $row['content'] . '</div>';                    
