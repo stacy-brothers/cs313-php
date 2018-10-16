9d085a9dfcae1f78dc212bc6cc1746381cb397b7
Stacy Brothers <bro18063@byui.edu>
2018-10-15 23:22:22 -0600

    week five project

--- web/week5/week5.php
+++ web/week5/week5.php
@@ -1,12 +1,57 @@
 <?php
 
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+
 ?>
 <html>
     <head>
-        
+        <title>CS 313 Assignments Portfolio</title>
+        <link rel="stylesheet" type="text/css" href="../index.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
     </head>
     <body>
-        
+        <div class="clearfix">
+            <div class="header">
+                <div class="header-container">
+                    <div class="header-text">Stacy Brothers - CS 313</div>                        
+                </div>
+            </div>
+            <div class="content">
+                <div class="page-title">Research Topics</div>
+               
+            </div>
+        </div>
+        <script type="text/javascript"> 
+            var keywords = [
+                <?php
+                    $comma = "";
+                    foreach ($db->query('select keyword from keyword k') as $row) {
+                        echo $comma . '"' . $row['keyword'] . '"';
+                    }
+                ?>
+            ];
+        </script>
     </body>
 </html>
+
 
