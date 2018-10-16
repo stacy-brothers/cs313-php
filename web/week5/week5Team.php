9f4ac5220665ab53fbf1977fbe56bf5c01edb0a0
Stacy Brothers <bro18063@byui.edu>
2018-10-15 22:38:55 -0600

    week 5 team stuff

--- web/week5/week5Team.php
+++ web/week5/week5Team.php
@@ -1,12 +1,55 @@
 <?php
 
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+
 ?>
 <html>
     <head>
-        
+        <title>CS 313 Assignments Portfolio</title>
+        <link rel="stylesheet" type="text/css" href="../index.css">
+        <link rel="stylesheet" type="text/css" href="./week5.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
     </head>
     <body>
-        
+        <div class="clearfix">
+            <div class="header">
+                <div class="header-container">
+                    <div class="header-text">Stacy Brothers - CS 313</div>                        
+                </div>
+            </div>
+            <div class="content">
+                <div class="page-title">Scripture Resources</div>
+            </div>
+            <div>
+                <?php
+                foreach ($db->query('SELECT book, chapter, verse, content FROM scripture') as $row) {
+                    echo '<div><b>' . $row['book'] . ' ' . $row['chapter'] . ':' . $row['verse'] . '</b> - ' . $row['content'] . '</div>';                    
+                }
+                ?>
+                
+                
+            </div>
+        </div>
     </body>
 </html>
 
