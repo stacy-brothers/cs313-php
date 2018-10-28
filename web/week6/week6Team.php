d95e37883e9b929bad2ebb2acfd027ec97972e23
Stacy Brothers <bro18063@byui.edu>
2018-10-27 20:15:13 -0600

    week 6 team project

--- web/week6/week6Team.php
+++ web/week6/week6Team.php
@@ -22,8 +22,9 @@ catch (PDOException $ex)
   die();
 }
 
-$book = $chapter = $verse = "";
-$bookErr = $chapterErr = $verseError = "";
+$book = $chapter = $verse = $content = "";
+$bookErr = $chapterErr = $verseError = $contentErr = "";
+$topics = [];
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
         error_log("Doing POST!");
@@ -31,7 +32,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $bookErr = "Book is required";
             $good = FALSE;
         } else {
-            $book = test_input($_POST["book"]);
+            $book = fix_input($_POST["book"]);
             if (!preg_match("/^[0-9a-zA-Z ]*$/",$book)) {
                 $bookErr = "Only numbers, letters and white space allowed";
                 $good = FALSE;
@@ -41,7 +42,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $chapterErr = "Chapter is required";
             $good = FALSE;
         } else {
-            $chapter = test_input($_POST["chapter"]);
+            $chapter = fix_input($_POST["chapter"]);
             if (!preg_match("/^[0-9]*$/",$chapter)) {
                 $chapterErr = "Only numbers allowed";
                 $good = FALSE;
@@ -51,14 +52,33 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $verseErr = "Verse is required";
             $good = FALSE;
         } else {
-            $verse = test_input($_POST["verse"]);
+            $verse = fix_input($_POST["verse"]);
+            if (!preg_match("/^[0-9]*$/",$verse)) {
+                $verseErr = "Only numbers allowed";
+                $good = FALSE;
+            }
+        }
+        if (empty($_POST["content"])) {
+            $contentErr = "Content is required";
+            $good = FALSE;
+        } else {
+            $content = fix_input($_POST["verse"]);
             if (!preg_match("/^[0-9]*$/",$verse)) {
                 $verseErr = "Only numbers allowed";
                 $good = FALSE;
             }
         }
         
+        $topics = $_POST["topics"];
+        
         error_log( "Adding: " . $book . " " . $chapter . ":" . $verse);
+    }
+    
+    function fix_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
     }
 
 ?>
@@ -79,23 +99,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 <div class="page-title">Add a new Scripture</div>
                 <form action="week6Team.php" method="POST">
                     <div>
-                        <div>Book <span class="error"><?=' - ' . $bookErr;?></span></div><div><input id='book' type="text" value="<?=$book?>"></div>
+                        <div>Book <span class="error"><?=' - ' . $bookErr;?></span></div><div><input name='book' id='book' type="text" value="<?=$book?>"></div>
                     </div>
                     <div>
-                        <div>Chapter <span class="error"><?=' - ' . $chapterErr;?></span></div><div><input id='chapter' type="text" value="<?=$chapter?>"></div>
+                        <div>Chapter <span class="error"><?=' - ' . $chapterErr;?></span></div><div><input name='chapter' id='chapter' type="text" value="<?=$chapter?>"></div>
                     </div>
                     <div>
-                        <div>Verse <span class="error"><?=' - ' . $verseErr;?></span></div><div><input id='verse' type="text" value="<?=$verse?>"></div>
+                        <div>Verse <span class="error"><?=' - ' . $verseErr;?></span></div><div><input name='verse' id='verse' type="text" value="<?=$verse?>"></div>
                     </div>
                     <div>
-                        <div>Content</div><div><textarea id='content' type="text" cols="50" rows="5"></textarea></div>
+                        <div>Content <span class="error"><?=' - ' . $contentErr;?></span></div><div><textarea name='content' id='content' type="text" cols="50" rows="5"><?=$content?></textarea></div>
                     </div>
                     <div>
                         <div>Topics</div>
                         <div>
                             <?php
                                 foreach ($db->query('SELECT id, name FROM script_topics') as $row) {
-                                    echo '<input type="checkbox" id="topics[]" value="' . $row['id'] . '">' . $row['name'] . ' ';                    
+                                    echo '<input type="checkbox" id="topics[]" value="' . $row['id'] . '" ' . ((isset($topics) && in_array($row['id'], $topics))?"checked":"") . '>' . $row['name'] . ' ';                    
                                 }
                             ?>
                         </div>
