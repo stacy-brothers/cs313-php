2ce31af2d231e694c7dca4bd490e59ef30af84ce
Stacy Brothers <bro18063@byui.edu>
2018-10-30 19:41:36 -0600

    week7

--- web/week7/weekTeamFinish.php
+++ web/week7/weekTeamFinish.php
@@ -9,7 +9,20 @@
 ?>
 
 <html>
+    <head>
+        <title>CS 313 Assignments Portfolio</title>
+        <link rel="stylesheet" type="text/css" href="../index.css">
+    </head>
     <body>
-        Welcome <?=$user?> 
+        <div class="clearfix">
+            <div class="header">
+                <div class="header-container">
+                    <div class="header-text">Stacy Brothers - CS 313</div>                        
+                </div>
+            </div>
+            <div class="content">
+                Welcome <?=$user?> 
+            </div>
+        </div>
     </body>
 </html>
