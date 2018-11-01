a2f46bf6dbe7ddae370aad0a8a55f55294b1f4f6
Stacy Brothers <bro18063@byui.edu>
2018-10-31 20:25:09 -0600

    week 7 project

--- web/project/detail.php
+++ web/project/detail.php
@@ -86,7 +86,7 @@ function fix_input($data) {
     <head>
         <title>CS 313 Assignments Portfolio</title>
         <link rel="stylesheet" type="text/css" href="../../index.css">
-        <link rel="stylesheet" type="text/css" href="detail.css">
+        <link rel="stylesheet" type="text/css" href="./detail.css">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
     </head>
     <body>
