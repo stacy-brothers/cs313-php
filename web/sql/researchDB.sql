c3068c681eed5054a01386fe17ead93831783408
Stacy Brothers <bro18063@byui.edu>
2018-11-01 21:12:37 -0600

    week 7 project

--- web/sql/researchDB.sql
+++ web/sql/researchDB.sql
@@ -2,31 +2,11 @@
 -- create database research;
 -- no permissions to create table in heroku postgres.... :(
 
--- users of the database
-create table researcher (
-    id serial primary key,
-    username varchar(30),
-    fullname varchar(50),
-    email varchar(50),
-    pass_hash varchar(254)
-);
-
 -- this is the main table 
 create table topic (
     id serial primary key,
     topic varchar(254) not null,
-    researcher_id integer references researcher(id),
-    private boolean default false,
     notes text
-);
-
--- this is a reference url for a particular topic.  
--- one topic may have many reference urls
-create table ref_url (
-    id serial primary key,
-    url varchar(254) not null, 
-    descr varchar(254),
-    topic_id integer references topic(id)
 );
 
 -- this is a lookup table that contains keywords that can be assigned to topics
